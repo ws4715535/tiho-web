e44f28dab948a6359aedca1a2af19d35d7f7d269
WwS <624412421@qq.com>
2025-12-16 13:01:32 +0800

    fix: rename

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export default function App() {
   useEffect(() => {
     const root = document.documentElement;
     // Reset all
-    root.classList.remove('dark', 'cyberpunk', 'forest');
+    root.classList.remove('dark', 'cyberpunk');
     
     if (theme === 'dark') {
       root.classList.add('dark');
